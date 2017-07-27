4963eb27d758136969dbe9b26d42463ca09e6438
z1dev <z-one@freemail.hu>
2017-07-27 08:44:46 +0200

    Converted all source files to UTF-8.

--- grammar.cpp
+++ grammar.cpp
@@ -1,5 +1,5 @@
 /*
-** Copyright 2007-2013, 2017 Sólyom Zoltán
+** Copyright 2007-2013, 2017 SÃ³lyom ZoltÃ¡n
 ** This file is part of zkanji, a free software released under the terms of the
 ** GNU General Public License version 3. See the file LICENSE for details.
 **/
@@ -273,7 +273,7 @@ const char *buinfromaji[]
         "banai", "baserareru", "bareru", "baseru",
         "bi", "bitai", "bitagaru", "bisou",
         "nde", "nda", "ndara", "ndari",
-        "beba", "beru", "be", "‚Úu",
+        "beba", "beru", "be", "â€šÃšu",
         "nderu", "ndeiru", "banakuya", "banakya",
         "njau", "banaide", "bazu", "banu",
         "basareru", "njimau", "ndenasai", "ndeoku",
--- popupsettings.h
+++ popupsettings.h
@@ -1,5 +1,5 @@
 /*
-** Copyright 2007-2013, 2017 SÛlyom Zolt·n
+** Copyright 2007-2013, 2017 S√≥lyom Zolt√°n
 ** This file is part of zkanji, a free software released under the terms of the
 ** GNU General Public License version 3. See the file LICENSE for details.
 **/
